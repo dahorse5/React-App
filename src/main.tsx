9d2284899f8bfdbed9d16c5bee68ecbf319b4b4d
or hazan <150969622+dahorse5@users.noreply.github.com>
2025-06-17 00:14:26 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './index.css';
-=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
->>>>>>> 7d0a02db6a21c1ebe91aecb85a8ede8645be4c20
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -24,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <ToastContainer position="top-right" autoClose={3000} theme="colored" />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 initThemeMode();
